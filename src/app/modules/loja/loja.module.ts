d4146329cdbbea0c5775674c6dc3947ec118e17b
Yuri Kevin <yurikevin1405@gmail.com>
2024-03-26 17:01:29 -0300

    feat: módulo de loja

---
+++ src/app/modules/loja/loja.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LojaRoutingModule } from './loja-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LojaRoutingModule
+  ]
+})
+export class LojaModule { }
