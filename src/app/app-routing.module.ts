dde37dd211d3d1335563cf66b7d0d693e5759904
Yuri Kevin <yurikevin1405@gmail.com>
2024-03-26 20:13:41 -0300

    rotas temporatias card e header para estilização css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/produto/home/home.component';
+import { ProdutoComponent } from './modules/produto/produto/produto.component';
+import { HeaderComponent } from './shared/header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'card', component: ProdutoComponent},
+  { path: 'header', component: HeaderComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
