6f52d026859d47622de8e2cf273dc0b96c2442e3
Yuri Kevin <yurikevin1405@gmail.com>
2024-03-26 17:01:39 -0300

    módulo de produto

---
+++ src/app/modules/produto/produto.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProdutoRoutingModule } from './produto-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProdutoRoutingModule
+  ]
+})
+export class ProdutoModule { }
