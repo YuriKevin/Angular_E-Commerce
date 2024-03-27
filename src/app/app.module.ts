63e52d71123568afdbc2d13252ad53463cf0ff35
Yuri Kevin <yurikevin1405@gmail.com>
2024-03-27 13:20:09 -0300

    declaração de novos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { HomeComponent } from './modules/produto/home/home.component';
+import { ProdutoComponent } from './modules/produto/produto/produto.component';
+import { SecaoComponent } from './shared/secao/secao.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    ProdutoComponent,
+    SecaoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
