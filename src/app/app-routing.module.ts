8123a213fd443df45c07e064bbffb44d760cc8f8
Yuri Kevin <yurikevin1405@gmail.com>
2024-03-27 13:19:42 -0300

    rotas temporárias para estilização de componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/produto/home/home.component';
 import { ProdutoComponent } from './modules/produto/produto/produto.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { SecaoComponent } from './shared/secao/secao.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'card', component: ProdutoComponent},
   { path: 'header', component: HeaderComponent},
+  { path: 'secao', component: SecaoComponent},
+  { path: 'footer', component: FooterComponent},
 ];
 
 @NgModule({
