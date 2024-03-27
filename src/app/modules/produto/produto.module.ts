61acd8e2edb248d4959a6d0dedb5f24876c4f60a
Yuri Kevin <yurikevin1405@gmail.com>
2024-03-27 18:34:15 -0300

    feat: declarando e exportando novos componentes

--- src/app/modules/produto/produto.module.ts
+++ src/app/modules/produto/produto.module.ts
@@ -1,19 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ProdutoRoutingModule } from './produto-routing.module';
 import { HomeComponent } from './home/home.component';
 import { ProdutoComponent } from './produto/produto.component';
-
+import { ListarComponent } from './listar/listar.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { EspecificacoesComponent } from './especificacoes/especificacoes.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
-    ProdutoComponent
+    ProdutoComponent,
+    ListarComponent,
+    EspecificacoesComponent
   ],
   imports: [
     CommonModule,
-    ProdutoRoutingModule
+    ProdutoRoutingModule,
+    SharedModule
+  ],
+  exports: [
+    ProdutoComponent,
+    ListarComponent,
+    HomeComponent,
+    EspecificacoesComponent
   ]
 })
 export class ProdutoModule { }
