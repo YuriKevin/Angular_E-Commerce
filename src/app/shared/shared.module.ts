740d794b43b7971bb5dbae4be4d508f1c7ea0edd
Yuri Kevin <yurikevin1405@gmail.com>
2024-03-27 18:36:43 -0300

    feat: novo módulo "Shared"

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedRoutingModule } from './shared-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SecaoComponent } from './secao/secao.component';
+
+
+@NgModule({
+    declarations: [
+        HeaderComponent,
+        FooterComponent,
+        SecaoComponent
+    ],
+    exports: [
+        HeaderComponent,
+        FooterComponent,
+        SecaoComponent
+    ],
+    imports: [
+        CommonModule,
+        SharedRoutingModule
+    ]
+})
+export class SharedModule { }
