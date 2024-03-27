df8544ba9b3d47dfe509bbfb7c01d69300a58af8
Yuri Kevin <yurikevin1405@gmail.com>
2024-03-27 13:22:22 -0300

    novo componente secao Component

---
+++ src/app/shared/secao/secao.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-secao',
+  templateUrl: './secao.component.html',
+  styleUrls: ['./secao.component.css']
+})
+export class SecaoComponent {
+
+}
