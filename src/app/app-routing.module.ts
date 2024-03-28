b9950c478eea24acef74e44179d62b5bef63ffcb
Yuri Kevin <yurikevin1405@gmail.com>
2024-03-28 11:56:49 -0300

    feat: novas rotas loja e listarLojas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,8 @@ import { SecaoComponent } from './shared/secao/secao.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { ListarComponent } from './modules/produto/listar/listar.component';
 import { EspecificacoesComponent } from './modules/produto/especificacoes/especificacoes.component';
+import { ListarLojasComponent } from './modules/loja/listar-lojas/listar-lojas.component';
+import { LojaComponent } from './modules/loja/loja/loja.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
@@ -16,6 +18,8 @@ const routes: Routes = [
   { path: 'footer', component: FooterComponent},
   { path: 'listar', component: ListarComponent},
   { path: 'especificacoes', component: EspecificacoesComponent},
+  { path: 'loja', component: LojaComponent},
+  { path: 'listarLojas', component: ListarLojasComponent},
 ];
 
 @NgModule({
