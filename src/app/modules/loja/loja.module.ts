6d265565ea5da24e04d22066160a86e02541aa12
Yuri Kevin <yurikevin1405@gmail.com>
2024-03-28 11:57:22 -0300

    feat: declaração de componentes

--- src/app/modules/loja/loja.module.ts
+++ src/app/modules/loja/loja.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LojaRoutingModule } from './loja-routing.module';
+import { LojaComponent } from './loja/loja.component';
+import { ListarLojasComponent } from './listar-lojas/listar-lojas.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LojaComponent,
+    ListarLojasComponent
+  ],
   imports: [
     CommonModule,
     LojaRoutingModule
+  ],
+  exports: [
+    LojaComponent,
+    ListarLojasComponent
   ]
 })
 export class LojaModule { }
