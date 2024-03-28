adffc56d95871b3217302f9291c8f24f51d32b92
Yuri Kevin <yurikevin1405@gmail.com>
2024-03-28 20:10:26 -0300

    feat: método para transformar arquivo de imagem para string base 64.

---
+++ src/app/modules/loja/cadastrar-loja/cadastrar-loja.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastrar-loja',
+  templateUrl: './cadastrar-loja.component.html',
+  styleUrls: ['./cadastrar-loja.component.css']
+})
+export class CadastrarLojaComponent {
+
+  logo!:string;
+
+  transformarLogoStringBase64(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    const files = inputElement?.files;
+    if (files && files.length > 0) {
+      const file = files[0];
+      if (this.verificarSeContemImagem(file)) {
+        this.transformarParaBase64String(file);
+      } else {
+        console.log("O arquivo selecionado não é uma imagem.");
+      }
+    }
+  }
+
+  verificarSeContemImagem(file: File): boolean {
+    return file.type.startsWith('image/');
+  }
+
+  transformarParaBase64String(file: File): void {
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.logo = reader.result as string;
+      console.log(this.logo);
+    };
+    reader.readAsDataURL(file);
+  }
+}
