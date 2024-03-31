0e646ce5d93fc9b624e576cc0ee1028f7ef71803
Yuri Kevin <yurikevin1405@gmail.com>
2024-03-31 14:31:07 -0300

    feat: novo componente cadastro-usuario

---
+++ src/app/modules/usuario/cadastro-usuario/cadastro-usuario.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-usuario',
+  templateUrl: './cadastro-usuario.component.html',
+  styleUrls: ['../login-usuario/login-usuario.component.css', './cadastro-usuario.component.css']
+})
+export class CadastroUsuarioComponent {
+
+}
