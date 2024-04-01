0f9bf6a8f66e0349aaddbc7650023219e55c88ac
Yuri Kevin <yurikevin1405@gmail.com>
2024-04-01 18:37:20 -0300

    feat: método login + atributos e construtor da classe.

--- src/app/modules/usuario/login-usuario/login-usuario.component.ts
+++ src/app/modules/usuario/login-usuario/login-usuario.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from 'src/app/interfaces/usuario';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import { FeedbackComponent } from 'src/app/shared/feedback/feedback.component';
 
 @Component({
   selector: 'app-login-usuario',
@@ -6,5 +10,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-usuario.component.css']
 })
 export class LoginUsuarioComponent {
+  email!:string;
+  senha!:string;
+  @ViewChild(FeedbackComponent) feedbackComponent!: FeedbackComponent;
 
+  constructor(private usuarioService:UsuarioService, private router:Router){
+
+  }
+  
+  login() {
+    if(!this.email || !this.senha){
+      this.feedbackComponent.open("Preencha todos os campos", true);
+      return;
+    }
+    this.feedbackComponent.open("Aguarde enquanto validamos seus dados.", false);
+    this.usuarioService.login(this.email, this.senha).subscribe({
+      next: (usuario:Usuario) => {
+        this.usuarioService.setUsuario(usuario);
+        this.router.navigate(['']);
+      },
+      error: (error) => {
+        this.feedbackComponent.open("Ocorreu um erro ao se conectar.", true)
+      }
+    });
+  }
 }
