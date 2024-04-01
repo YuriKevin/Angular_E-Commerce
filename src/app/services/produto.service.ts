cf3fdf251e0179cc076eb749a4694174c63cb886
Yuri Kevin <yurikevin1405@gmail.com>
2024-04-01 18:33:29 -0300

    feat: método encontrarPorId + atributos e construtor da classe.

--- src/app/services/produto.service.ts
+++ src/app/services/produto.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Produto } from '../interfaces/produto';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdutoService {
+  private apiURL = "http://localhost:8080/";
 
-  constructor() { }
+  httpOptions = {
+    headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+    })
+  }
+  
+  constructor(private httpClient: HttpClient, private router:Router) {
+  }
+  
+  encontrarPorId(id: number): Observable<any> {
+    return this.httpClient.get<Produto>(this.apiURL + 'produto/' + id)
+    .pipe(
+      catchError((error: HttpErrorResponse) => {
+        let errorMessage = 'Erro desconhecido';
+        if (error.error instanceof ErrorEvent) {
+          // Erro do cliente
+          errorMessage = `Erro: ${error.error.message}`;
+        } else {
+          // Erro do servidor
+          errorMessage = error.error.message;
+        }
+        return throwError(errorMessage);
+      })
+    );
+  }
 }
