2a55c8b78f3c4b86bffba81b12cb4202a76a4438
Yuri Kevin <yurikevin1405@gmail.com>
2024-04-01 18:35:55 -0300

    feat: método cadastrar + atributos e construtor da classe

--- src/app/modules/usuario/cadastro-usuario/cadastro-usuario.component.ts
+++ src/app/modules/usuario/cadastro-usuario/cadastro-usuario.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from 'src/app/interfaces/usuario';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import { FeedbackComponent } from 'src/app/shared/feedback/feedback.component';
 
 @Component({
   selector: 'app-cadastro-usuario',
@@ -6,5 +10,34 @@ import { Component } from '@angular/core';
   styleUrls: ['../login-usuario/login-usuario.component.css', './cadastro-usuario.component.css']
 })
 export class CadastroUsuarioComponent {
+  email!:string;
+  nome!:string;
+  senha!:string;
+  confirmarSenha!:string;
 
+  @ViewChild(FeedbackComponent) feedbackComponent!: FeedbackComponent;
+
+  constructor(private usuarioService:UsuarioService, private router:Router){
+
+  }
+  cadastrar(){
+    if(!this.email || !this.nome || !this.senha || !this.confirmarSenha){
+      this.feedbackComponent.open("Preencha todos os campos", true);
+      return;
+    }
+    else if(this.senha != this.confirmarSenha){
+      this.feedbackComponent.open("Senhas não coincidem.", true);
+      return;
+    }
+    this.feedbackComponent.open("Aguarde enquanto validamos seus dados.", false);
+    this.usuarioService.cadastrar(this.email, this.nome, this.senha).subscribe({
+      next: (usuario:Usuario) => {
+        this.usuarioService.setUsuario(usuario);
+        this.router.navigate(['/loginUsuario']);
+      },
+      error: (error) => {
+        this.feedbackComponent.open("Ocorreu um erro ao se cadastrar.", true)
+      }
+    });
+  }
 }
