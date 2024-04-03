9e23162b49708e44d1d3aa9e3a706b18f5a0254c
Yuri Kevin <yurikevin1405@gmail.com>
2024-04-03 17:41:34 -0300

    feat: métodos -> getCarrinho, removerProdutoCarrinho, adicionarProdutoCarrinho, adicionarHistoricoNavegacao, pesquisarHistorico, pesqusiarProdutos, recentes, produtosAmazing

--- src/app/services/produto.service.ts
+++ src/app/services/produto.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Produto } from '../interfaces/produto';
@@ -9,6 +9,9 @@ import { Observable, catchError, throwError } from 'rxjs';
 })
 export class ProdutoService {
   private apiURL = "http://localhost:8080/";
+  carrinho: Produto[] = [];
+  historicoNavegacao:string[] = [];
+  produtosHistorico:Produto[] = [];
 
   httpOptions = {
     headers: new HttpHeaders({
@@ -55,6 +58,114 @@ export class ProdutoService {
 
   produtosDestaque(): Observable<any> {
     return this.httpClient.get<Produto>(this.apiURL + 'produto/destaques')
+    .pipe(
+      catchError((error: HttpErrorResponse) => {
+        let errorMessage = 'Erro desconhecido';
+        if (error.error instanceof ErrorEvent) {
+          // Erro do cliente
+          errorMessage = `Erro: ${error.error.message}`;
+        } else {
+          // Erro do servidor
+          errorMessage = error.error.message;
+        }
+        return throwError(errorMessage);
+      })
+    );
+  }
+
+
+  getCarrinho(){
+    return this.carrinho;
+  }
+  adicionarProdutoCarrinho(produto:Produto){
+    const index = this.carrinho.findIndex(produto => produto.id === produto.id);
+    if (index !== -1) {
+      this.carrinho[index].quantidade += produto.quantidade;
+    }
+    else{
+      this.carrinho.push(produto);
+    }
+  }
+
+  removerProdutoCarrinho(produto:Produto){
+    const index = this.carrinho.findIndex(item => item.id === produto.id);
+    if (index !== -1) {
+      this.carrinho.splice(index, 1);
+    }
+  }
+
+  adicionarHistoricoNavegacao(palavraPesquisada:string){
+    if (palavraPesquisada.endsWith(' ')) {
+      palavraPesquisada = palavraPesquisada.trimEnd();
+    }
+    if (!this.historicoNavegacao.includes(palavraPesquisada)) {
+      this.historicoNavegacao.push(palavraPesquisada);
+    }
+  }
+
+  temHistorico():boolean{
+    if(this.historicoNavegacao.length==0){
+      return false
+    }
+    return true;
+  }
+
+  pesquisarHistorico(): Observable<any> {
+    const params = new HttpParams()
+          .set('palavrasPesquisadas', this.historicoNavegacao.join(','));
+    return this.httpClient.get<Produto>(this.apiURL + 'produto/historico', { params })
+    .pipe(
+      catchError((error: HttpErrorResponse) => {
+        let errorMessage = 'Erro desconhecido';
+        if (error.error instanceof ErrorEvent) {
+          // Erro do cliente
+          errorMessage = `Erro: ${error.error.message}`;
+        } else {
+          // Erro do servidor
+          errorMessage = error.error.message;
+        }
+        return throwError(errorMessage);
+      })
+    );
+  }
+
+  pesquisarProdutos(titulo:string, pagina:number): Observable<Produto[]> {
+    const params = new HttpParams().set('pagina', pagina.toString());
+    return this.httpClient.get<Produto[]>(`${this.apiURL}produto/titulo/${titulo}`, { params })
+    .pipe(
+      catchError((error: HttpErrorResponse) => {
+        let errorMessage = 'Erro desconhecido';
+        if (error.error instanceof ErrorEvent) {
+          // Erro do cliente
+          errorMessage = `Erro: ${error.error.message}`;
+        } else {
+          // Erro do servidor
+          errorMessage = error.error.message;
+        }
+        return throwError(errorMessage);
+      })
+    );
+  }
+
+  recentes(): Observable<any> {
+    return this.httpClient.get<Produto>(this.apiURL + 'produto/recentes')
+    .pipe(
+      catchError((error: HttpErrorResponse) => {
+        let errorMessage = 'Erro desconhecido';
+        if (error.error instanceof ErrorEvent) {
+          // Erro do cliente
+          errorMessage = `Erro: ${error.error.message}`;
+        } else {
+          // Erro do servidor
+          errorMessage = error.error.message;
+        }
+        return throwError(errorMessage);
+      })
+    );
+  }
+
+  produtosAmazing(): Observable<any> {
+    return this.httpClient.get<Produto>(this.apiURL + 'produto/recentes')
     .pipe(
       catchError((error: HttpErrorResponse) => {
         let errorMessage = 'Erro desconhecido';
