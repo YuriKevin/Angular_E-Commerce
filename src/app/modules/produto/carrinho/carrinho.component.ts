c82e90d0e5a55521d5946d9eb8c686dc17bb6010
Yuri Kevin <yurikevin1405@gmail.com>
2024-04-03 17:32:55 -0300

    feat: atributos + métodos removerProdutoCarrinho e ngOnInit

--- src/app/modules/produto/carrinho/carrinho.component.ts
+++ src/app/modules/produto/carrinho/carrinho.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Produto } from 'src/app/interfaces/produto';
+import { Usuario } from 'src/app/interfaces/usuario';
+import { ProdutoService } from 'src/app/services/produto.service';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-carrinho',
   templateUrl: './carrinho.component.html',
   styleUrls: ['./carrinho.component.css']
 })
-export class CarrinhoComponent {
+export class CarrinhoComponent implements OnInit{
+  compraRealizada:boolean = false;
+  produtos!:Produto[];
+  usuario!:Usuario;
 
+  constructor(private produtoService: ProdutoService, private usuarioService: UsuarioService){}
+
+  ngOnInit(): void {
+      this.produtos = this.produtoService.getCarrinho();
+      if(this.produtos.length == 0){
+
+      }
+  }
+
+  removerProdutoCarrinho(produto:Produto){
+    this.produtoService.removerProdutoCarrinho(produto);
+  }
+
+  
+  
 }
