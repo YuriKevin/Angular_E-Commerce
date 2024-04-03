f0248459447df78ac512da7f7dc6ad25a6081b74
Yuri Kevin <yurikevin1405@gmail.com>
2024-04-03 17:37:58 -0300

    feat: atributo produtos + construtor + método ngOnInit

---
+++ src/app/modules/produto/recentes/recentes.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Produto } from 'src/app/interfaces/produto';
+import { ProdutoService } from 'src/app/services/produto.service';
+
+@Component({
+  selector: 'app-recentes',
+  templateUrl: './recentes.component.html',
+  styleUrls: ['./recentes.component.css']
+})
+export class RecentesComponent implements OnInit{
+  produtos!:Produto[];
+
+  constructor(private produtoService:ProdutoService){}
+
+  ngOnInit(): void {
+    this.produtoService.maisVendidos().subscribe({
+      next: (produtos:Produto[]) => {
+        this.produtos = produtos;
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
+  }
+
+}
