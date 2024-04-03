13358927bcf935767e6b7039ff8ef0e087cc367d
Yuri Kevin <yurikevin1405@gmail.com>
2024-04-03 18:01:07 -0300

    feat: atributos + construtor + métodos adicionarCreditos e ngOnInit

--- src/app/modules/usuario/creditos/creditos.component.ts
+++ src/app/modules/usuario/creditos/creditos.component.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from 'src/app/interfaces/usuario';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-creditos',
   templateUrl: './creditos.component.html',
   styleUrls: ['./creditos.component.css']
 })
-export class CreditosComponent {
+export class CreditosComponent implements OnInit{
+  usuario!:Usuario;
+  creditosAdicionados!:number;
+
+  constructor(private usuarioService: UsuarioService, private router: Router){}
+
+  ngOnInit(): void {
+      this.usuario = this.usuarioService.getUsuario();
+      if(!this.usuario){
+        this.router.navigate(['/loginUsuario', true]);
+      }
+  }
+  adicionarCreditos(){
+    if(this.creditosAdicionados){
+      this.usuarioService.adicionarCreditos(this.usuario.id, this.creditosAdicionados).subscribe({
+        next: () => {
+          this.router.navigate(['/carrinho']);
+          this.usuarioService.atualizarCredito(this.creditosAdicionados);
+        },
+        error: (error) => {
+
+        }
+      });
+    }
+  }
 }
