b75b28745de588b4a4a1ac26b44d58347af9cf8d
Yuri Kevin <yurikevin1405@gmail.com>
2024-04-03 17:31:34 -0300

    feat: métodos pesquisarLojas, passarPagina, retrocederPagina e ngOnInit

--- src/app/modules/loja/listar-lojas/listar-lojas.component.ts
+++ src/app/modules/loja/listar-lojas/listar-lojas.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LojaDTO } from 'src/app/interfaces/loja-dto';
+import { LojaService } from 'src/app/services/loja.service';
 
 @Component({
   selector: 'app-listar-lojas',
   templateUrl: './listar-lojas.component.html',
   styleUrls: ['./listar-lojas.component.css']
 })
-export class ListarLojasComponent {
+export class ListarLojasComponent implements OnInit{
+  lojas!:LojaDTO[];
+  nomeLoja!:string;
+  paginacao:number=0;
 
+  constructor(private lojaService:LojaService){}
+
+  ngOnInit(): void {
+      this.lojaService.listarLojas(this.paginacao).subscribe({
+        next: (lojas:LojaDTO[]) => {
+          this.lojas = lojas;
+          console.log(lojas);
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      });
+  }
+
+  pesquisarLojas(){
+    if(this.nomeLoja){
+      this.paginacao = 0;
+      this.lojaService.pesquisarLojas(this.nomeLoja, this.paginacao).subscribe({
+        next: (lojas:LojaDTO[]) => {
+          this.lojas = lojas;
+          console.log(lojas);
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      });
+    }
+  }
+
+  passarPagina(){
+    if(this.lojas.length==18){
+      this.paginacao += 1;
+      this.lojaService.pesquisarLojas(this.nomeLoja, this.paginacao).subscribe({
+        next: (lojas:LojaDTO[]) => {
+          this.lojas = lojas;
+          console.log(lojas);
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      });
+    }
+  }
+  retrocederPagina(){
+    if(this.paginacao>0){
+      this.paginacao -= 1;
+      this.lojaService.pesquisarLojas(this.nomeLoja, this.paginacao).subscribe({
+        next: (lojas:LojaDTO[]) => {
+          this.lojas = lojas;
+          console.log(lojas);
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      });
+    }
+  }
 }
