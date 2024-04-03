6d8999ed412303a79e1cb379fb6fe3005573656b
Yuri Kevin <yurikevin1405@gmail.com>
2024-04-03 17:36:39 -0300

    feat: atributos + construtor + métodos mudarPagina e ngOnInit

--- src/app/modules/produto/pesquisar/pesquisar.component.ts
+++ src/app/modules/produto/pesquisar/pesquisar.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Produto } from 'src/app/interfaces/produto';
+import { ProdutoService } from 'src/app/services/produto.service';
 
 @Component({
   selector: 'app-pesquisar',
   templateUrl: './pesquisar.component.html',
   styleUrls: ['./pesquisar.component.css']
 })
-export class PesquisarComponent {
 
+export class PesquisarComponent {
+  palavraPesquisada!: string;
+  pagina!: number;
+  produtos!:Produto[];
+  numeroPaginaUsuario!:number;
+
+  constructor(private route: ActivatedRoute, private produtoService:ProdutoService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.palavraPesquisada = params['palavra'];
+      this.pagina = + params['pagina'];
+      this.numeroPaginaUsuario = this.pagina + 1;
+      this.produtoService.adicionarHistoricoNavegacao(this.palavraPesquisada);
+      this.produtoService.pesquisarProdutos(this.palavraPesquisada, this.pagina).subscribe({
+        next: (produtos:Produto[]) => {
+          this.produtos = produtos;
+        },
+        error: (error) => {
+          //feedback
+        }
+      });
+    });
+  }
+  mudarPagina(avancar:boolean){
+    if(avancar){
+      this.pagina++;
+      this.router.navigate(['/pesquisar', this.palavraPesquisada, this.pagina]);
+    }
+    else{
+      if(this.pagina!=0){
+        this.pagina--;
+        this.router.navigate(['/pesquisar', this.palavraPesquisada, this.pagina]);
+      }
+    }
+    
+  }
 }
