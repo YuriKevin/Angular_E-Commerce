3fc48a82c53340c6e231b86f9bd707fb53f45e7a
Yuri Kevin <yurikevin1405@gmail.com>
2024-04-04 16:06:17 -0300

    feat: método realizar compra

--- src/app/modules/produto/carrinho/carrinho.component.ts
+++ src/app/modules/produto/carrinho/carrinho.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Produto } from 'src/app/interfaces/produto';
 import { Usuario } from 'src/app/interfaces/usuario';
+import { ProdutoCompradoService } from 'src/app/services/produto-comprado.service';
 import { ProdutoService } from 'src/app/services/produto.service';
 import { UsuarioService } from 'src/app/services/usuario.service';
 
@@ -14,19 +16,32 @@ export class CarrinhoComponent implements OnInit{
   produtos!:Produto[];
   usuario!:Usuario;
 
-  constructor(private produtoService: ProdutoService, private usuarioService: UsuarioService){}
+  constructor(private router:Router, private produtoService: ProdutoService, private usuarioService:UsuarioService,private produtoCompradoService: ProdutoCompradoService){}
 
   ngOnInit(): void {
       this.produtos = this.produtoService.getCarrinho();
-      if(this.produtos.length == 0){
-
-      }
+      this.usuario = this.usuarioService.getUsuario();
   }
 
   removerProdutoCarrinho(produto:Produto){
     this.produtoService.removerProdutoCarrinho(produto);
   }
 
-  
+  realizarCompra(){
+    if(!this.usuario){
+      this.router.navigate(['/loginUsuario', true]);
+    }
+    else if(this.produtos && this.produtos.length != 0){
+      
+      this.produtoCompradoService.novaCompra(this.usuario.id, this.produtos).subscribe({
+        next: (produto:Produto) => {
+          this.compraRealizada = true;
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      });
+    }
+  }
   
 }
