9e4a4d67c507143df8657a63324a64554dc3f587
Yuri Kevin <yurikevin1405@gmail.com>
2024-04-04 16:11:06 -0300

    feat: imports + ngOnInit

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Usuario } from 'src/app/interfaces/usuario';
 import { ProdutoService } from 'src/app/services/produto.service';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
   palavraPesquisada!:string;
+  usuario!:Usuario;
   
-  constructor(private produtoService: ProdutoService, private router: Router){}
+  constructor(private produtoService: ProdutoService, private router: Router, private usuarioService:UsuarioService){}
+
+  ngOnInit(): void {
+      this.usuario = this.usuarioService.getUsuario();
+  }
 
   pesquisar(){
     if(this.palavraPesquisada && this.palavraPesquisada !=''){
