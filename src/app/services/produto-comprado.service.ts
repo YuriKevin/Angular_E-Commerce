81ae28b3efe35c3fb82a8937f7d5e299b0dae7dc
Yuri Kevin <yurikevin1405@gmail.com>
2024-04-04 16:09:13 -0300

    feat: métodos -> novaCompra, carregarCompras, avaliarProdutos + construtor e atributos do serviço

--- src/app/services/produto-comprado.service.ts
+++ src/app/services/produto-comprado.service.ts
@@ -1,9 +1,81 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Produto } from '../interfaces/produto';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdutoCompradoService {
+  private apiURL = "http://localhost:8080/";
 
-  constructor() { }
+  httpOptions = {
+    headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+    })
+  }
+  
+  constructor(private httpClient: HttpClient, private router:Router) {
+  }
+
+  novaCompra(id:number, produtosArray:Produto[]): Observable<any> {
+    const produtos: { produtoId: number, quantidade: number }[] = [];
+    produtosArray.forEach(produto => {
+      const { id: produtoId, quantidade } = produto;
+      produtos.push({ produtoId, quantidade });
+    });
+    console.log(produtos);
+    return this.httpClient.post<any>(`${this.apiURL}compra/${id}`, produtos)
+    .pipe(
+      catchError((error: HttpErrorResponse) => {
+        let errorMessage = 'Erro desconhecido';
+        if (error.error instanceof ErrorEvent) {
+          // Erro do cliente
+          errorMessage = `Erro: ${error.error.message}`;
+        } else {
+          // Erro do servidor
+          errorMessage = error.error.message;
+        }
+        return throwError(errorMessage);
+      })
+    );
+  }
+
+  carregarCompras(usuarioId:number, pagina:number): Observable<any> {
+    const params = new HttpParams().set('pagina', pagina);
+    return this.httpClient.get<any>(`${this.apiURL}compra/usuario/${usuarioId}`, { params })
+    .pipe(
+      catchError((error: HttpErrorResponse) => {
+        let errorMessage = 'Erro desconhecido';
+        if (error.error instanceof ErrorEvent) {
+          // Erro do cliente
+          errorMessage = `Erro: ${error.error.message}`;
+        } else {
+          // Erro do servidor
+          errorMessage = error.error.message;
+        }
+        return throwError(errorMessage);
+      })
+    );
+  }
+
+  avaliarProduto(id:number, avaliacao:number): Observable<any> {
+    const params = new HttpParams().set('avaliacao', avaliacao);
+    return this.httpClient.put<any>(`${this.apiURL}compra/avaliarProduto/${id}`, {}, { params })
+    .pipe(
+      catchError((error: HttpErrorResponse) => {
+        let errorMessage = 'Erro desconhecido';
+        if (error.error instanceof ErrorEvent) {
+          // Erro do cliente
+          errorMessage = `Erro: ${error.error.message}`;
+        } else {
+          // Erro do servidor
+          errorMessage = error.error.message;
+        }
+        return throwError(errorMessage);
+      })
+    );
+  }
+
 }
