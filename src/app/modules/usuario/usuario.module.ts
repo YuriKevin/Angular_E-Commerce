ae9e791655912db464baab71f5b870f2d51884f8
Yuri Kevin <yurikevin1405@gmail.com>
2024-04-05 14:56:43 -0300

    delete: remoção de compras component

--- src/app/modules/usuario/usuario.module.ts
+++ src/app/modules/usuario/usuario.module.ts
@@ -5,7 +5,6 @@ import { UsuarioRoutingModule } from './usuario-routing.module';
 import { CreditosComponent } from './creditos/creditos.component';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { ComprasComponent } from './compras/compras.component';
 import { LoginUsuarioComponent } from './login-usuario/login-usuario.component';
 import { CadastroUsuarioComponent } from './cadastro-usuario/cadastro-usuario.component';
 import { AtualizarSenhaUsuarioComponent } from './atualizar-senha-usuario/atualizar-senha-usuario.component';
@@ -16,7 +15,6 @@ import { AtualizarDadosComponent } from './atualizar-dados/atualizar-dados.compo
 @NgModule({
   declarations: [
     CreditosComponent,
-    ComprasComponent,
     LoginUsuarioComponent,
     CadastroUsuarioComponent,
     AtualizarSenhaUsuarioComponent,
@@ -31,7 +29,6 @@ import { AtualizarDadosComponent } from './atualizar-dados/atualizar-dados.compo
   ],
   exports: [
     CreditosComponent,
-    ComprasComponent,
     LoginUsuarioComponent,
     CadastroUsuarioComponent,
     AtualizarSenhaUsuarioComponent
