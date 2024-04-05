4e81b01e6d6468909c5b2224ba401ccad1c7c37b
Yuri Kevin <yurikevin1405@gmail.com>
2024-04-05 14:55:10 -0300

    feat: novo componente paginacao

---
+++ src/app/shared/paginacao/paginacao.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paginacao',
+  templateUrl: './paginacao.component.html',
+  styleUrls: ['./paginacao.component.css']
+})
+export class PaginacaoComponent {
+
+}
