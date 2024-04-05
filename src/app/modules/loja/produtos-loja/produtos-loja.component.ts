ed7cb76cce2741aedb1c3d6ff71c8620b5ce0697
Yuri Kevin <yurikevin1405@gmail.com>
2024-04-05 14:52:04 -0300

    feat: método pesquisar, mudarPagina e ngOnInit + atributos e construtor da classe

--- src/app/modules/loja/produtos-loja/produtos-loja.component.ts
+++ src/app/modules/loja/produtos-loja/produtos-loja.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { Produto } from 'src/app/interfaces/produto';
+import { ProdutoService } from 'src/app/services/produto.service';
 
 @Component({
   selector: 'app-produtos-loja',
   templateUrl: './produtos-loja.component.html',
-  styleUrls: ['./produtos-loja.component.css']
+  styleUrls: ['../../../shared/paginacao/paginacao.component.css', './produtos-loja.component.css']
 })
-export class ProdutosLojaComponent {
+export class ProdutosLojaComponent implements OnInit{
+  produtos!:Produto[];
+  lojaId!:number;
+  pagina!:number;
+  numeroPaginaUsuario!:number;
+  tituloInput!:string;
+  mostrarPaginacao: boolean = true;
 
+  constructor(private route: ActivatedRoute, private produtoService:ProdutoService, private router: Router) { }
+   
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.lojaId = params['id'];
+      this.pagina = + params['pagina'];
+      console.log(this.pagina);
+      this.numeroPaginaUsuario = this.pagina + 1;
+      this.produtoService.pesquisarProdutosDeUmaLoja(this.lojaId, this.pagina).subscribe({
+        next: (produtos:Produto[]) => {
+          this.produtos = produtos;
+          console.log(this.produtos);
+          if(this.produtos.length<18){
+            this.mostrarPaginacao=false;
+          }
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      });
+    });
+      
+  }
+
+  pesquisar(){
+    if(this.tituloInput){
+      this.produtoService.pesquisarProdutosDeUmaLojaPorTitulo(this.lojaId, this.tituloInput, this.pagina).subscribe({
+        next: (produtos:Produto[]) => {
+          this.produtos = produtos;
+          console.log(this.produtos);
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      });
+  }
+  }
+
+  mudarPagina(avancar:boolean){
+    if(avancar){
+      if(this.produtos.length==18){
+        this.pagina++;
+        this.router.navigate(['/produtosLoja', this.lojaId, this.pagina]);
+      }
+    }
+    else{
+      if(this.pagina!=0){
+        this.pagina--;
+        this.router.navigate(['/produtosLoja', this.lojaId, this.pagina]);
+      }
+    }
+  }
 }
