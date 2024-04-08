0a113bcc88e1741258c0dbf616306fe638b87ec9
Yuri Kevin <yurikevin1405@gmail.com>
2024-04-08 18:39:32 -0300

    feat: métodos -> atualizarProduto, atualizarSenha, atualizarDados, cancelarCompra, adicionarCategoria, removerCategoria, listarCategoriasDeUmaLoja

--- src/app/services/loja.service.ts
+++ src/app/services/loja.service.ts
@@ -6,6 +6,8 @@ import { Router } from '@angular/router';
 import { Loja } from '../interfaces/loja';
 import { DetalhesProduto } from '../interfaces/detalhes-produto';
 import { LojaDTO } from '../interfaces/loja-dto';
+import { Produto } from '../interfaces/produto';
+import { Categoria } from '../interfaces/categoria';
 
 @Injectable({
   providedIn: 'root'
@@ -22,7 +24,7 @@ export class LojaService {
   }
 
   constructor(private httpClient: HttpClient, private router:Router) {
-    this.login(1111, "1234").subscribe({
+    this.login(1111, "12345").subscribe({
       next: (loja:Loja) => {
         this.setLoja(loja);
         console.log("funcionou");
@@ -137,5 +139,163 @@ export class LojaService {
         })
       );
     }
-    
+
+    atualizarProduto(produto:Produto): Observable<any>{
+      return this.httpClient.put(this.apiURL+'produto', produto)
+      .pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 404) {
+          return throwError('Nenhum usuário encontrado');
+        } else if(error.status === 400) {
+          return throwError('Usuário ou senha incorretos');
+        }
+        else {
+          let errorMessage = 'Erro ao carregar usuário';
+          if (error.error instanceof ErrorEvent) {
+            errorMessage = `Erro no cliente: ${error.error.message}`;
+          } else {
+            errorMessage = `Erro no servidor, tente novamente mais tade.`;
+            errorMessage = `Erro no servidor, tente novamente mais tade: ${error.status}, `;
+          }
+          return throwError(errorMessage);
+        }
+      })
+    );
+  }
+
+  atualizarSenha(id:number, senha:string, senhaAntiga:string): Observable<any>{
+    const params = new HttpParams()
+    .set('id', id.toString())
+    .set('senha', senha)
+    .set('senhaAntiga', senhaAntiga);
+    return this.httpClient.put(this.apiURL + 'loja/atualizarSenha', {}, { params })
+      .pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 404) {
+          return throwError('Nenhum usuário encontrado');
+        } else if(error.status === 400) {
+          return throwError('Usuário ou senha incorretos');
+        }
+        else {
+          let errorMessage = 'Erro ao carregar usuário';
+          if (error.error instanceof ErrorEvent) {
+            errorMessage = `Erro no cliente: ${error.error.message}`;
+          } else {
+            errorMessage = `Erro no servidor, tente novamente mais tade.`;
+            errorMessage = `Erro no servidor, tente novamente mais tade: ${error.status}, `;
+          }
+          return throwError(errorMessage);
+        }
+      })
+    );
+  }
+
+  atualizarDados(id:number, codigoLogin:number, nome:string, logo:string): Observable<any>{
+    return this.httpClient.put(this.apiURL+'loja', {id, codigoLogin, nome, logo})
+    .pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 404) {
+        return throwError('Nenhum usuário encontrado');
+      } else if(error.status === 400) {
+        return throwError('Usuário ou senha incorretos');
+      }
+      else {
+        let errorMessage = 'Erro ao carregar usuário';
+        if (error.error instanceof ErrorEvent) {
+          errorMessage = `Erro no cliente: ${error.error.message}`;
+        } else {
+          errorMessage = `Erro no servidor, tente novamente mais tade.`;
+          errorMessage = `Erro no servidor, tente novamente mais tade: ${error.status}, `;
+        }
+        return throwError(errorMessage);
+      }
+    })
+  );
+}
+  cancelarCompra(id:number): Observable<any>{
+    return this.httpClient.put(this.apiURL+'compra/cancelarCompra/'+ id, {})
+    .pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 404) {
+        return throwError('Nenhum usuário encontrado');
+      } else if(error.status === 400) {
+        return throwError('Usuário ou senha incorretos');
+      }
+      else {
+        let errorMessage = 'Erro ao carregar usuário';
+        if (error.error instanceof ErrorEvent) {
+          errorMessage = `Erro no cliente: ${error.error.message}`;
+        } else {
+          errorMessage = `Erro no servidor, tente novamente mais tade.`;
+          errorMessage = `Erro no servidor, tente novamente mais tade: ${error.status}, `;
+        }
+        return throwError(errorMessage);
+      }
+    })
+  );
+  }
+
+  adicionarCategoria(id:number, titulo:string, idsProdutos:number[]): Observable<any>{
+    const body = { titulo, idsProdutos };
+    return this.httpClient.put(`${this.apiURL}loja/adicionarCategoria/${id}`, body)
+      .pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 404) {
+          return throwError('Nenhum usuário encontrado');
+        } else if(error.status === 400) {
+          return throwError('Usuário ou senha incorretos');
+        }
+        else {
+          let errorMessage = 'Erro ao carregar usuário';
+          if (error.error instanceof ErrorEvent) {
+            errorMessage = `Erro no cliente: ${error.error.message}`;
+          } else {
+            errorMessage = `Erro no servidor, tente novamente mais tade.`;
+            errorMessage = `Erro no servidor, tente novamente mais tade: ${error.status}, `;
+          }
+          return throwError(errorMessage);
+        }
+      })
+    );
+  }
+
+  removerCategoria(lojaId:number, categoriaId:number): Observable<any>{
+    return this.httpClient.delete(`${this.apiURL}loja/removerCategoria/${lojaId}/${categoriaId}`)
+      .pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 404) {
+          return throwError('Nenhum usuário encontrado');
+        } else if(error.status === 400) {
+          return throwError('Usuário ou senha incorretos');
+        }
+        else {
+          let errorMessage = 'Erro ao carregar usuário';
+          if (error.error instanceof ErrorEvent) {
+            errorMessage = `Erro no cliente: ${error.error.message}`;
+          } else {
+            errorMessage = `Erro no servidor, tente novamente mais tade.`;
+            errorMessage = `Erro no servidor, tente novamente mais tade: ${error.status}, `;
+          }
+          return throwError(errorMessage);
+        }
+      })
+    );
+  }
+
+  listarCategoriasDeUmaLoja(id:number):Observable<any> {
+    return this.httpClient.get<LojaDTO>(`${this.apiURL}produto/categorias/${id}`)
+    .pipe(
+      catchError((error: HttpErrorResponse) => {
+        let errorMessage = 'Erro desconhecido';
+        if (error.error instanceof ErrorEvent) {
+          // Erro do cliente
+          errorMessage = `Erro: ${error.error.message}`;
+        } else {
+          // Erro do servidor
+          errorMessage = error.error.message;
+        }
+        return throwError(errorMessage);
+      })
+    );
+  }
 }
