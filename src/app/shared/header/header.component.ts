45d4eb788853460bfee9bc981c29bb2a2f5cf7df
Yuri Kevin <yurikevin1405@gmail.com>
2024-04-08 18:41:48 -0300

    feat: setar loja, método abrirCategorias

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Loja } from 'src/app/interfaces/loja';
 import { Usuario } from 'src/app/interfaces/usuario';
+import { LojaService } from 'src/app/services/loja.service';
 import { ProdutoService } from 'src/app/services/produto.service';
 import { UsuarioService } from 'src/app/services/usuario.service';
 
@@ -12,11 +14,14 @@ import { UsuarioService } from 'src/app/services/usuario.service';
 export class HeaderComponent implements OnInit{
   palavraPesquisada!:string;
   usuario!:Usuario;
+  loja!:Loja;
+  categorias:boolean = false;
   
-  constructor(private produtoService: ProdutoService, private router: Router, private usuarioService:UsuarioService){}
+  constructor(private produtoService: ProdutoService, private lojaService: LojaService, private router: Router, private usuarioService:UsuarioService){}
 
   ngOnInit(): void {
       this.usuario = this.usuarioService.getUsuario();
+      this.loja = this.lojaService.getLoja();
   }
 
   pesquisar(){
@@ -24,5 +29,14 @@ export class HeaderComponent implements OnInit{
       this.produtoService.adicionarHistoricoNavegacao(this.palavraPesquisada);
       this.router.navigate(['/pesquisar', this.palavraPesquisada, 0]);
     }
+  }
+
+  abrirCategorias(){
+    if(!this.categorias){
+      this.categorias = true;
+    }else{
+      this.categorias = false;
+    }
+    
   }
 }
